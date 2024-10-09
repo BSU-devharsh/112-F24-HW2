3cfcfb4e07db4c695c989727d8a078d6fdbe98d5
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2024-10-09 17:50:25 -0400

    Create StringAnalysis.java

---
+++ StringAnalysis.java
@@ -0,0 +1,19 @@
+// Write Your Name Here
+
+import java.util.Scanner;
+
+public class StringAnalysis {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your code below
+        // ...
+
+        // Display the results
+        System.out.println("Length of the string: " + length);
+        System.out.println("Number of vowels: " + vowelCount);
+        System.out.println("Starts with a capital letter: " + startsWithCapital);
+        System.out.println("Contains \"hello\": " + containsHello);
+        System.out.println("Last occurrence of 'e': " + lastEIndex);
+    }
+}
